c75f42beb69ddb05569ab50496084b498dbdd009
Ratel-Surface <callaghanljgmail.com>
2017-02-22 14:35:14 -0800

    Working RSA encryptor

--- RSAProject/RSAProject/Form1.Designer.cs
+++ RSAProject/RSAProject/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RSAProject
+﻿namespace RSAEncryption
 {
     partial class Form1
     {
@@ -28,21 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtencrypt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtdecrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtplain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtencrypt
+            // 
+            this.txtencrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtencrypt.ForeColor = System.Drawing.Color.Red;
+            this.txtencrypt.Location = new System.Drawing.Point(12, 185);
+            this.txtencrypt.Multiline = true;
+            this.txtencrypt.Name = "txtencrypt";
+            this.txtencrypt.Size = new System.Drawing.Size(564, 68);
+            this.txtencrypt.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(582, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 68);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtdecrypt
+            // 
+            this.txtdecrypt.Location = new System.Drawing.Point(12, 278);
+            this.txtdecrypt.Multiline = true;
+            this.txtdecrypt.Name = "txtdecrypt";
+            this.txtdecrypt.Size = new System.Drawing.Size(645, 68);
+            this.txtdecrypt.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(582, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 68);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtplain
+            // 
+            this.txtplain.Location = new System.Drawing.Point(12, 92);
+            this.txtplain.Multiline = true;
+            this.txtplain.Name = "txtplain";
+            this.txtplain.Size = new System.Drawing.Size(564, 68);
+            this.txtplain.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Encrypted Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(120, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(418, 36);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Asymmetric Encryption-RSA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Plain Text For Encryption";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Plain Text After Decryption";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(669, 368);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtencrypt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtdecrypt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtplain);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RSA Algorithm";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtencrypt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtdecrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtplain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
+
 
--- RSAProject/RSAProject/Program.cs
+++ RSAProject/RSAProject/Program.cs
@@ -1,62 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 using System.Windows.Forms;
 
-namespace RSAProject
+namespace RSAEncryption
 {
-    class Program
+    static class Program
     {
-        UnicodeEncoding ByteConverter = new UnicodeEncoding();
-        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
-        byte[] plaintext;
-        byte[] encryptedtext;
-        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] encryptedData;
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAKey);
-                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
-                }
-                return encryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Things are here");
-                return null;
-            }
-        }
-
-        static public byte[] Decryption(byte[] Data, RSAParameters RSAkey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] decryptedData;
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    RSA.ImportParameters(RSAkey);
-                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
-                }
-                return decryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.ToString());
-                return null;
-            }
-        }
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// I changed some stuff here.
         [STAThread]
         static void Main()
         {
