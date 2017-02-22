8127cd58b27fbdf84eee54b6bbdcf0bee735e4bd
Adam <vezina@unbc.ca>
2017-02-22 15:00:25 -0800

    Commit after merge.

--- RSAProject/RSAProject/Program.cs
+++ RSAProject/RSAProject/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 using System.Windows.Forms;
 
-namespace RSAProject
+namespace RSAEncryption
 {
+    static class Program
+    {
     class Program
     {
         UnicodeEncoding ByteConverter = new UnicodeEncoding();
@@ -18,25 +16,23 @@ namespace RSAProject
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// I changed some stuff here.
         [STAThread]
         static void Main()
         {
-            using (RSACryptoServiceProvider playtest = new RSACryptoServiceProvider(1024))
-            {
-                string message = "Alice and Bob";
-                byte[] unencoded_message = Encoding.ASCII.GetBytes(message);
-                byte[] encoded_message = playtest.Encrypt(unencoded_message, false);
-                RSAParameters parameters = playtest.ExportParameters(true);
-                byte[] p = parameters.P;
-                if (BitConverter.IsLittleEndian)
-                    Array.Reverse(p);
+                using (RSACryptoServiceProvider playtest = new RSACryptoServiceProvider(1024))
+                {
+                    string message = "Alice and Bob";
+                    byte[] unencoded_message = Encoding.ASCII.GetBytes(message);
+                    byte[] encoded_message = playtest.Encrypt(unencoded_message, false);
+                    RSAParameters parameters = playtest.ExportParameters(true);
+                    byte[] p = parameters.P;
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(p);
 
-                int converted_bytes = BitConverter.ToInt32(p, 0);
+                    int converted_bytes = BitConverter.ToInt32(p, 0);
 
-                Console.WriteLine("End.");
+                    Console.WriteLine("End.");
+                }
             }
-            
-        }
     }
 }
