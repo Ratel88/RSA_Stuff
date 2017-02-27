6583cf05c492a0b492104d4733eb33b2f1eed626
Adam <vezina@unbc.ca>
2017-02-27 15:32:50 -0800

    Added HelperTools class to convert ByteArrays to a String for easier comparison and visualization.

---
+++ RSAProject/RSAProject/HelperTools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperTools
+{
+    class ByteArrayToString
+    {
+        public static string convert(Byte[] array)
+        {
+            BigInteger final_number = 0;
+            BigInteger exponent_calculator = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                exponent_calculator = BigInteger.Pow(array[i], i);
+                final_number += exponent_calculator;
+            }
+
+            return final_number.ToString();
+        }
+    }
+}
